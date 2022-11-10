d9ca842fd949235678e1863d85afe69bed85cc10
Chudi <chudiibida3@gmail.com>
2022-11-10 11:22:40 -0500

    added styled components to category

--- src/components/Goals/Category/Category.js
+++ src/components/Goals/Category/Category.js
@@ -1,9 +1,30 @@
+import styled from 'styled-components';
 
+const colorMatchup = {
+    'lifestyle': '#EF626C',
+    'work': '#464D77',
+    'fitness': '#85C7F2',
+    'hobby': '#EFD780',
+    'finances': '#9CD08F',
+    'project': '#5C6672',
+}
+
+const Block = styled.div(props => ({
+    border: '1px solid black',
+    backgroundColor: colorMatchup[props.color]
+}))
+
+// const categoryStyle = {
+//     backgroundColor: colorMatchup[type],
+// }
 
 const Category = ({ type }) => {
     return (
         <div className="category">
-            {type}
+
+            <Block color={type}>
+                {type}
+            </Block>
         </div>
     )
 }
